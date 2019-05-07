e011f036cf35eb9cba08ce32e507afe1e6a62662
HubCodes <krlrhkstk12@gmail.com>
2019-05-07 22:05:01 +0900

    Testing

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,5 @@
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin registry.entrydsm.hs.kr:5000
 
 docker build -t hubcodes/shuck .
-docker push hubcodes/shuck
+docker tag hubcodes/shuck hubcodes/shuck:latest
+docker push hubcodes/shuck:latest
