d4e8573e0e42e28c39032b3926513c118bc2947c
HubCodes <krlrhkstk12@gmail.com>
2019-05-07 20:28:53 +0900

    Testing

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (_, res) => {
   res.send("Hello, world!");
 });
 
+app.get("/hello", (_, res) => {
+  res.send("Good job.");
+});
+
 const server = app.listen(20800, () => {
   console.info("[INFO] Server listening at 20800");
 });
