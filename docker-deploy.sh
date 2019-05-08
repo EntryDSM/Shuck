a091a0e862f84841b93369f969db4161e8037ddc
HubCodes <krlrhkstk12@gmail.com>
2019-05-08 09:44:12 +0900

    Testing

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,7 +1,7 @@
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin registry.entrydsm.hs.kr:5000
 
-docker build -t entrydsm/shuck:0.0.1 .
-docker tag entrydsm/shuck:0.0.1 entrydsm/shuck:latest
+docker build -t registry.entrydsm.hs.kr:5000/shuck:0.0.1 .
+docker tag registry.entrydsm.hs.kr:5000/shuck:0.0.1 registry.entrydsm.hs.kr:5000/shuck:latest
 
-docker push entrydsm/shuck:0.0.1
-docker push entrydsm/shuck:latest
+docker push registry.entrydsm.hs.kr:5000/shuck:0.0.1 
+docker push registry.entrydsm.hs.kr:5000/shuck:latest
